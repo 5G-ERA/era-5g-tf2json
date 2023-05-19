8f57a68f8b4168cce8cfb3ff08dca2e117452280
ZdenekM <imaterna@fit.vutbr.cz>
2023-05-19 13:33:16 +0200

    feat: first working version

---
+++ examples/node.py
@@ -0,0 +1,27 @@
+import rclpy
+from geometry_msgs.msg import TransformStamped
+from rclpy.node import Node
+
+from era_5g_tf2json.tf2_web_republisher import TFRepublisher
+
+
+class ExampleNode(Node):
+    def __init__(self) -> None:
+        super(ExampleNode, self).__init__("tf2_web_republisher_example_node")
+        self.republisher = TFRepublisher(self, self.callback, 1.0)
+
+    def callback(self, transforms: TransformStamped) -> None:
+        self.get_logger().info(f"Got {len(transforms)}")
+
+
+def main(args=None) -> None:
+    rclpy.init(args=args)
+
+    node = ExampleNode()
+    node.republisher.subscribe_transform("robot", "world", 0.1, 0.001)
+    node.republisher.subscribe_transform("camera", "world", 0.1, 0.001)
+    rclpy.spin(node)
+
+
+if __name__ == "__main__":
+    main()
